b58b634b5d5104a98210549dfaf03f42196492df
marques-9 <122379309+marques-9@users.noreply.github.com>
2023-01-26 20:59:26 -0300

    Add files via upload

---
+++ src/js/index.js
@@ -0,0 +1,19 @@
+const botaoTrailer = document.querySelector(".botao-trailer");
+const botaoFecharModal = document.querySelector(".fechar-modal");
+const modal = document.querySelector(".modal");
+const video = document.getElementById("video");
+
+function alternarModal(){
+    modal.classList.toggle("aberto");
+}
+
+botaoTrailer.addEventListener("click", () => {
+    alternarModal();
+    video.setAttribute("src", "https://www.youtube.com/embed/Cb4WV4aXBpk");
+});
+
+botaoFecharModal.addEventListener("click", () => {
+    alternarModal();
+    video.setAttribute("src", "");
+});
+
